b3b0a0cc2e3f0d4cf3e333dfe4adf0fff3597f83
gdm-9961 <159160385+gdm-9961@users.noreply.github.com>
2025-03-08 12:08:10 -0500

    Added StageMeta
    abstract class for stage-specific properties

---
+++ Back End Memories/Assets/Scripts/StageMeta.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StageMeta : MonoBehaviour
+{
+    int tileCount; // height of level = number of tiles to be generated
+    int[] tileStack; // array storing semirandomly generated tile id's, so that they can be despawned and recalled
+
+    int batteryCount; // number of batteries to be generated
+    float[][] batteries; // array storing battery coordinates and remaining power: {X,Y,Power}
+
+    private void Start()
+    {
+        // initialize tileStack to size tileCount
+        // prepopulate 0 and length-1, then semirandomly generate the rest
+
+        PopulateBatteries();
+    }
+
+    private void PopulateBatteries()
+    {
+        // initialize batteryCount based on tileCount and difficulty level
+        // generate coordinates for initial battery spawns
+
+        // will need to update coordinates and remove 
+    }
+
+
+}
