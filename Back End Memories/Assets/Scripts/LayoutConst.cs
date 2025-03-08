0d33577466228eac214fd6cc56764626cb15ea12
gdm-9961 <159160385+gdm-9961@users.noreply.github.com>
2025-03-08 12:09:01 -0500

    Update LayoutConst.cs
    Changed to struct, replaced ints with doubles

--- Back End Memories/Assets/Scripts/LayoutConst.cs
+++ Back End Memories/Assets/Scripts/LayoutConst.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class LayoutConst
+public struct LayoutConst
 {
     // update values when we agree on a standard, use to simplify calculations
-    public const int TileH = 1; // height of tiles for stage layout
-    public const int TileInnerW = 2; // width of middle (traversable) tile for stage layout
-    public const int TileOuterW = 1; // width of outer (wall) tiles for stage layout
+    public const double TileH = 1.0; // height of tiles for stage layout
+    public const double TileInnerW = 2.0; // width of middle (traversable) tile for stage layout
+    public const double TileOuterW = 1.0; // width of outer (wall) tiles for stage layout
 }
