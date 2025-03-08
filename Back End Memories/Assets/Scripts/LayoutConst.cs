22b456d9fc5d51e641c1fb96cb34dd984483452a
gdm-9961 <159160385+gdm-9961@users.noreply.github.com>
2025-03-08 11:00:37 -0500

    Added LayoutConst
    placeholder static class containing constants for use in stage layout calculations

---
+++ Back End Memories/Assets/Scripts/LayoutConst.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LayoutConst
+{
+    // update values when we agree on a standard, use to simplify calculations
+    public const int TileH = 1; // height of tiles for stage layout
+    public const int TileInnerW = 2; // width of middle (traversable) tile for stage layout
+    public const int TileOuterW = 1; // width of outer (wall) tiles for stage layout
+}
