73825abbe9a59e5f9df525cc392c4639f360fb8e
gdm-9961 <159160385+gdm-9961@users.noreply.github.com>
2025-04-10 16:25:54 -0400

    incomplete platform changes

--- Back End Memories/Assets/Scripts/PlatformScript.cs
+++ Back End Memories/Assets/Scripts/PlatformScript.cs
@@ -13,7 +13,7 @@ public class PlatformScript : MonoBehaviour
 //x position platform starts at
     private float xStart;
     private float yBase;
-    private float yDirection;
+    public float yDirection;
     private float xDirection;
     private Rigidbody2D player;
     private int remainingWait = 0;
@@ -22,41 +22,67 @@ public class PlatformScript : MonoBehaviour
     //Array of possible positions on x axis
     private static float[] xLocations = { -4f, -.5f, 3f };
     private static int lastX = 0;
-    private static int platforms = 0;
-    private static int maxPlatforms = 5;
-    private static float initialY;
-    private static float yOffset = -6f;
+    private static int platformCount = 0;
+    private static int maxPlatforms = pSpawner.maxPlatforms;
+    private static PlatformScript[] platforms = new PlatformScript[maxPlatforms];
+    private static float lastY;
+    private static bool lastStart = false;
+    private static float yOffset = 6f;
     private static int waitFrames = 60;
 
 // Start is called before the first frame update
     void Start()
     {
         platformRB = GetComponent<Rigidbody2D>();
-        platformId = platforms++;
-
-        // setting y coordinate
-        if (platformId == 0) yBase = initialY = platformRB.position.y;
-        else yBase = initialY + platformId * yOffset;
-
-        float yStart = UnityEngine.Random.Range(yBase,yBase+5);
-
-        // setting movement direction
-        if (platformId % 2 == UnityEngine.Random.Range(0,3))
+        platformId = platformCount;
+        if (platformCount++ == platforms.Length)
         {
-            yDirection = 0;
-            xDirection = 1;
-            yBase += yOffset/2;
+            PlatformScript[] npl = new PlatformScript[platformCount * 2];
+            for (int i = 0; i < platformCount; i++) npl[i] = platforms[i];
+            platforms = npl;
+        }
+        platforms[platformId] = this;
+
+        float yStart;
+        // setting y coordinate
+        if (platformId == 0)
+        {
+            yBase = platformRB.position.y;
+            yDirection = 1;
+            xDirection = 0;
+            lastY = yBase - (yOffset - 2f);
+            yStart = yBase - (yOffset / 2);
+            lastStart = true;
         }
         else
         {
-            yDirection = 1;
-            xDirection = 0;
-        }
+            if (platformId % 2 == UnityEngine.Random.Range(0, 3)) 
+            {
+                xDirection = platformId % 2 == 1 ? 1 : -1;
+                yDirection = 0;
+            }
+            else {
+                xDirection = 0;
+                yDirection = platformId % 2 == 1 ? 1 : -1;
+            }
 
+            if (yDirection == 0)
+            {
+                lastY -= yOffset + 2f;
+                yStart = yBase = lastY;
+            }
+            else
+            {
+                yBase = lastY - 2f;
+                lastY -= yOffset;
+                yStart = yBase - (yOffset / 2) - (yOffset / 4 * yDirection);
+            }
+        }
+        
         // choosing semirandom X position to start
         if (xDirection != 0)
         {
-            transform.position = new Vector2(UnityEngine.Random.Range(xLocations[0],xLocations[xLocations.Length-1]),yStart);
+            transform.position = new Vector2(UnityEngine.Random.Range(xLocations[0],xLocations[xLocations.Length-1]),yBase);
         }
         else 
         {
@@ -66,8 +92,6 @@ public class PlatformScript : MonoBehaviour
             
             transform.position = new Vector2(xLocations[lastX],yStart);
         }
-
-        if (platformId < maxPlatforms) Instantiate(this);
     }
 
 // Update is called once per frame
@@ -100,14 +124,14 @@ public class PlatformScript : MonoBehaviour
         if (yDirection != 0)
         {
             //When yPos exceeds the bounds; record position and reverse motion
-            if (transform.position.y > yBase + 5)
+            if (transform.position.y > yBase)
             {
                 yDirection = -1;
                 waitCooldown = remainingWait = waitFrames;
                 platformRB.velocity = new Vector2(0,0);
                 if (player != null) player.velocity = new Vector2(player.velocity.x, 0);
             }
-            else if (transform.position.y < yBase-.1f){
+            else if (transform.position.y < yBase-(yOffset-2f)){
                 yDirection = 1;
                 waitCooldown = remainingWait = waitFrames;
                 platformRB.velocity = new Vector2(0,0);
@@ -115,13 +139,13 @@ public class PlatformScript : MonoBehaviour
         }
         else if (xDirection != 0)
         {
-            if (transform.position.x < xLocations[0]-.1f)
+            if (transform.position.x < xLocations[0]+.9f)
             {
                 xDirection = 1;
                 waitCooldown = remainingWait = waitFrames;
                 platformRB.velocity = new Vector2(0f,0f);
             }
-            else if (transform.position.x > xLocations[xLocations.Length-1]+.1f){
+            else if (transform.position.x > xLocations[xLocations.Length-1]-.9f){
                 xDirection = -1;
                 waitCooldown = remainingWait = waitFrames;
                 platformRB.velocity = new Vector2(0f,0f);
--- Back End Memories/Assets/Scripts/pSpawner.cs
+++ Back End Memories/Assets/Scripts/pSpawner.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class pSpawner : MonoBehaviour
 {
+    public GameObject movingPlatform;
+    public static int maxPlatforms = 6;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        int platformCount = 0;
+        Instantiate(movingPlatform, GetComponent<Rigidbody2D>().position,Quaternion.identity);
+        while (++platformCount < maxPlatforms) Instantiate(movingPlatform);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
